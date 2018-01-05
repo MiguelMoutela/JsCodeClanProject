ddbc18f1b47f1b192925998d5047e1d919425fb5
Marta <martadabrowka@gmail.com>
2018-01-05 21:16:54 +0000

    added whereAmI function

--- client/public/mapWrapper.js
+++ client/public/mapWrapper.js
@@ -5,3 +5,15 @@ const MapWrapper = function (container, coords, zoom) {
   });
   // this.markers = []
 }
+
+
+MapWrapper.prototype.whereAmI = function() {
+ navigator.geolocation.getCurrentPosition(function(position) {
+   const location = {
+     lat: position.coords.latitude,
+     lng: position.coords.longitude
+   }
+   this.googleMap.setCenter(location);
+   this.addMarker(location, 'This is your current location');
+ }.bind(this))
+}
