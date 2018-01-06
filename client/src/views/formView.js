3d97ad17c5092e04d8e744e7efa43d4f72912d53
Joao <johnynecas@hotmail.com>
2018-01-06 22:16:20 +0000

    created first sketch of funtion to load form

--- client/src/views/formView.js
+++ client/src/views/formView.js
@@ -1,9 +1,20 @@
+const newPageView = require('/newPageView.js');
+const Request = require('./services/request.js');
+
+// const newBody= new newPageView();
+// not sure if I shpould put this variable out or just call it on each function
+
 const formView = function(){
 
 }
 
 formView.prototype.viewCitySearch= function(){
+ const newBody= new newPageView();
+ const form = document.querySelector('#event-selection-form');
+ 
 
 
 
 }
+
+module.exports = formView;
