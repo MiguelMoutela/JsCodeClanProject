1015b5f04836a46a825490d108d4ee0e910b199b
Joao <johnynecas@hotmail.com>
2018-01-09 09:28:45 +0000

    tuesday morning nothing changed just comkit for new develop pull

--- client/public/bundle.js
+++ client/public/bundle.js
@@ -326,9 +326,72 @@ document.addEventListener('DOMContentLoaded', app);
 
 /***/ }),
 /* 3 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module parse failed: Unexpected token (47:7)\nYou may need an appropriate loader to handle this file type.\n|         const request = new Request(searchUrl);\n| \n|       }.bind(this), function() {\n|         alert('Not able search');\n|       };");
+const NewPageView = __webpack_require__(1);
+const Request = __webpack_require__(0);
+
+const FormView = function(){
+
+}
+
+//
+// FormView.prototype.searchByCity= function(event){
+//   event.preventDefault();
+//
+//   const inputCity = document.querySelector('#city').value;
+//   const categoryList = document.querySelector('#categories_list');
+//   categoryList.addEventListener('change', function (event) {
+//     const categorySelected= event.target.value;
+//     const searchUrl = `http://api.eventful.com/json/events/search?app_key=ZpGXZc399XdxLZG9&location=${inputCity}&category=${categorySelected}&date=Future`;
+//
+//     const request = new Request(searchUrl);
+    //request.get(function(allEvents){
+
+    //})
+    // not absolutly sure of this bellow well or above
+    // const outcomeObject = request.get();
+    // return outcomeObject
+//   });
+// }
+//
+//   //not sure about the binding here..is it required or not? same for function needing event
+//   FormView.prototype.searchAroundMe= function(event){
+//     event.preventDefault();
+//
+//     if(navigator.geolocation){
+//       navigator.geolocation.getCurrentPosition(function(position) {
+//         const lat= position.coords.latitude;
+//         const lng= position.coords.longitude;
+//
+
+
+//       });
+//
+//       const categoryList = document.querySelector('#categories_list');
+//       categoryList.addEventListener('change', function (event) {
+//         const categorySelected= event.target.value;
+//       })
+//
+//         const radius = document.querySelector('#radius').value;
+//
+//         const searchUrl = `
+//         http://api.eventful.com/json/events/search?app_key=ZpGXZc399XdxLZG9&where=${lat},${lng}&within=${radius}&category=${categorySelected}`;
+//
+//         const request = new Request(searchUrl);
+//
+//       }.bind(this), function() {
+//         alert('Not able search');
+//       };
+//     }
+//     else{
+//       alert('You do not have geolocation available on your device');
+//     }
+//   }
+
+
+    module.exports = FormView;
+
 
 /***/ }),
 /* 4 */
--- client/src/views/formView.js
+++ client/src/views/formView.js
@@ -5,53 +5,59 @@ const FormView = function(){
 
 }
 
+//
+// FormView.prototype.searchByCity= function(event){
+//   event.preventDefault();
+//
+//   const inputCity = document.querySelector('#city').value;
+//   const categoryList = document.querySelector('#categories_list');
+//   categoryList.addEventListener('change', function (event) {
+//     const categorySelected= event.target.value;
+//     const searchUrl = `http://api.eventful.com/json/events/search?app_key=ZpGXZc399XdxLZG9&location=${inputCity}&category=${categorySelected}&date=Future`;
+//
+//     const request = new Request(searchUrl);
+    //request.get(function(allEvents){
 
-FormView.prototype.searchByCity= function(event){
-  event.preventDefault();
-
-  const inputCity = document.querySelector('#city').value;
-  const categoryList = document.querySelector('#categories_list');
-  categoryList.addEventListener('change', function (event) {
-    const categorySelected= event.target.value;
-    const searchUrl = `http://api.eventful.com/json/events/search?app_key=ZpGXZc399XdxLZG9&location=${inputCity}&category=${categorySelected}&date=Future`;
-
-    const request = new Request(searchUrl);
+    //})
     // not absolutly sure of this bellow well or above
     // const outcomeObject = request.get();
     // return outcomeObject
-  });
-}
+//   });
+// }
+//
+//   //not sure about the binding here..is it required or not? same for function needing event
+//   FormView.prototype.searchAroundMe= function(event){
+//     event.preventDefault();
+//
+//     if(navigator.geolocation){
+//       navigator.geolocation.getCurrentPosition(function(position) {
+//         const lat= position.coords.latitude;
+//         const lng= position.coords.longitude;
+//
 
-  //not sure about the binding here..is it required or not? same for function needing event
-  FormView.prototype.searchAroundMe= function(event){
-    event.preventDefault();
 
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(function(position) {
-        const lat= position.coords.latitude;
-        const lng= position.coords.longitude;
-      });
-
-      const categoryList = document.querySelector('#categories_list');
-      categoryList.addEventListener('change', function (event) {
-        const categorySelected= event.target.value;
-      })
-
-        const radius = document.querySelector('#radius').value;
-
-        const searchUrl = `
-        http://api.eventful.com/json/events/search?app_key=ZpGXZc399XdxLZG9&where=${lat},${lng}&within=${radius}&category=${categorySelected}`;
-
-        const request = new Request(searchUrl);
-
-      }.bind(this), function() {
-        alert('Not able search');
-      };
-    }
-    else{
-      alert('You do not have geolocation available on your device');
-    }
-  }
+//       });
+//
+//       const categoryList = document.querySelector('#categories_list');
+//       categoryList.addEventListener('change', function (event) {
+//         const categorySelected= event.target.value;
+//       })
+//
+//         const radius = document.querySelector('#radius').value;
+//
+//         const searchUrl = `
+//         http://api.eventful.com/json/events/search?app_key=ZpGXZc399XdxLZG9&where=${lat},${lng}&within=${radius}&category=${categorySelected}`;
+//
+//         const request = new Request(searchUrl);
+//
+//       }.bind(this), function() {
+//         alert('Not able search');
+//       };
+//     }
+//     else{
+//       alert('You do not have geolocation available on your device');
+//     }
+//   }
 
 
     module.exports = FormView;
