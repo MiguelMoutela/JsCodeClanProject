195ddc7ba97e47d3fd0dbc95a12216ed915a897f
Joao <johnynecas@hotmail.com>
2018-01-10 09:22:12 +0000

    commented out code on route function.to work on later

--- client/src/views/mapWrapper.js
+++ client/src/views/mapWrapper.js
@@ -111,34 +111,145 @@ MapWrapper.prototype.displayEventMarkers = function(object) {
     const markerInfo = this.addMarkerInfoWindow(content);
 
     const marker =  this.addMarker(coords);
+
     marker.addListener('click', function() {
       markerInfo.open(this.map, marker);
-      // const autocomplete = new google.maps.places.autocomplete(DOM_NODE);
-      // console.log(autocomplete);
-      // autocomplete.bindTo('bounds', this.map);
-      // autocomplete.addListener('place_changed', function(){
-      //   const place = autocomplete.getPlace();
-      // });
 
-      const CalculateAndRenderRoute = function(origin,destination){
-        const directionsService = new google.maps.directionsService();
-        const  directionsDisplay = new google.maps.DirectionsRenderer();
-        request = {
-          origin : origin,
-          destination: destination
+    });
 
-        }
+    // marker.addListener('dblclick', function() {
+    //   var directionsDisplay = new google.maps.DirectionsRenderer();
+    //   var directionsService = new google.maps.DirectionsService();
+    //   // arrays to hold copies of the markers and html used by the side_bar
+    //   // because the function closure trick doesnt work there
+    //   var gmarkers = [];
+    //   var htmls = [];
+    //
+    //   // arrays to hold variants of the info window html with get direction forms open
+    //   var to_htmls = [];
+    //   var from_htmls = [];
+    //
+    //   // global "map" variable
+    //   var map = null;
+    //
+    //   var infowindow = new google.maps.InfoWindow({
+    //     size: new google.maps.Size(150, 50)
+    //   });
+    //
+    //   function initialize() {
+    //
+    //     var location = new google.maps.LatLng(50.871622, -4.131561);
+    //
+    //     var mapOptions = {
+    //       center: location,
+    //       zoom: 11,
+    //       scrollwheel: false
+    //     };
+    //
+    //     map = new google.maps.Map(document.getElementById("map"),
+    //     mapOptions);
+    //
+    //     directionsDisplay.setMap(map);
+    //     directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+    //     google.maps.event.addListener(map, 'click', function() {
+    //       infowindow.close();
+    //     });
+    //
+    //     var image = {
+    //       url: 'http://maps.google.com/mapfiles/ms/micons/blue.png'
+    //     };
+    //     var marker = new google.maps.Marker({
+    //       position: location,
+    //       map: map,
+    //       animation: google.maps.Animation.DROP,
+    //       icon: image,
+    //       title: 'Deer Park Dairy'
+    //     });
+    //
+    //     var i = gmarkers.length;
+    //     latlng = location;
+    //
+    //     // The info window version with the "to here" form open
+    //     to_htmls[i] = html + '<br>Directions: <b>To here<\/b> - <a href="javascript:fromhere(' + i + ')">From here<\/a>' +
+    //     '<br>Start address:<form action="javascript:getDirections()">' +
+    //     '<input type="text" SIZE=40 MAXLENGTH=40 name="saddr" id="saddr" value="" /><br>' +
+    //     '<INPUT value="Get Directions" TYPE="button" onclick="getDirections()"><br>' +
+    //     'Walk <input type="checkbox" name="walk" id="walk" /> &nbsp; Avoid Highways <input type="checkbox" name="highways" id="highways" />' +
+    //     '<input type="hidden" id="daddr" value="' + latlng.lat() + ',' + latlng.lng() +
+    //     '"/>';
+    //     // The info window version with the "from here" form open
+    //     from_htmls[i] = html + '<br>Directions: <a href="javascript:tohere(' + i + ')">To here<\/a> - <b>From here<\/b>' +
+    //     '<br>End address:<form action="javascript:getDirections()">' +
+    //     '<input type="text" SIZE=40 MAXLENGTH=40 name="daddr" id="daddr" value="" /><br>' +
+    //     '<INPUT value="Get Directions" TYPE="SUBMIT"><br>' +
+    //     'Walk <input type="checkbox" name="walk" id="walk" /> &nbsp; Avoid Highways <input type="checkbox" name="highways" id="highways" />' +
+    //     '<input type="hidden" id="saddr" value="' + latlng.lat() + ',' + latlng.lng() +
+    //     '"/>';
+    //     // The inactive version of the direction info
+    //     var html = marker.getTitle() + '<br>Directions: <a href="javascript:tohere(' + i + ')">To here<\/a> - <a href="javascript:fromhere(' + i + ')">From here<\/a>';
+    //     var contentString = html;
+    //
+    //     google.maps.event.addListener(marker, 'click', function() {
+    //       map.setZoom(15);
+    //       map.setCenter(marker.getPosition());
+    //       infowindow.setContent(contentString);
+    //       infowindow.open(map, marker);
+    //     });
+    //     // save the info we need to use later for the side_bar
+    //     gmarkers.push(marker);
+    //     htmls[i] = html;
+    //   }
+    //
+    //   google.maps.event.addDomListener(window, 'load', initialize);
+    //
+    //   // ===== request the directions =====
+    //   function getDirections() {
+    //     // ==== Set up the walk and avoid highways options ====
+    //     var request = {};
+    //     if (document.getElementById("walk").checked) {
+    //       request.travelMode = google.maps.DirectionsTravelMode.WALKING;
+    //     } else {
+    //       request.travelMode = google.maps.DirectionsTravelMode.DRIVING;
+    //     }
+    //
+    //     if (document.getElementById("highways").checked) {
+    //       request.avoidHighways = true;
+    //     }
+    //     // ==== set the start and end locations ====
+    //     var saddr = document.getElementById("saddr").value;
+    //     var daddr = document.getElementById("daddr").value;
+    //
+    //     request.origin = saddr;
+    //     request.destination = daddr;
+    //     directionsService.route(request, function(response, status) {
+    //       if (status == google.maps.DirectionsStatus.OK) {
+    //         directionsDisplay.setDirections(response);
+    //       } else alert("Directions not found:" + status);
+    //     });
+    //   }
+    //   function myclick(i) {
+    //     google.maps.event.trigger(gmarkers[i], "click");
+    //   }
+    //
+    //
+    //   // functions that open the directions forms
+    //   function tohere(i) {
+    //     // gmarkers[i].openInfoWindowHtml(to_htmls[i]);
+    //     infowindow.setContent(to_htmls[i]);
+    //     infowindow.open(map, gmarkers[i]);
+    //   }
+    //
+    //   function fromhere(i) {
+    //     // gmarkers[i].openInfoWindowHtml(from_htmls[i]);
+    //     infowindow.setContent(from_htmls[i]);
+    //     infowindow.open(map, gmarkers[i]);
+    //   }
+    //
+    // });
 
-        directionsDisplay.setMap(this.map);
-        directionsService.route(request,function(result,status){
-          if(status==='OK'){
-            directionsDisplay.setDirections(result);
-          }
 
-        })
 
-      }
-    })
+
   }
 }
 
